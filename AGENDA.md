a3eea98ad6a44e78fdd6da53867b457d1ef1e1b8
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2025-02-13 21:41:26 +0000

    feat: minimal test err

--- AGENDA.md
+++
@@ -1,5 +0,0 @@
-
-1. Features
-2. How to install
-3. Advantages over using Dataform UI
-4. How to contribute
--- includes/docs.js
+++
@@ -1,7 +0,0 @@
-const columnDescriptions = {
- player_id: "unique identifier for a player"
-}
-
-module.exports = {
- columnDescriptions,
-}
